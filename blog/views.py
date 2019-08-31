be38f4440d5ec36ca608d1ec1ddcc2b5d8dd3bdf
aito <aitolivelive@gmail.com>
2019-08-31 15:13:03 +0600

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
 from blog.models import Post
 from django.utils import timezone
 
 def get_index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'index.html', {'posts': posts})
+    return render(request,'index.html',{"posts": posts})
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {})
+
+def new_post(request):
+    pass
